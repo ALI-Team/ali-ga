e8d385bea1e8a27463db56b57be0b4f837c99230
Luka Jankovic <lukjan1999@gmail.com>
2017-11-16 21:07:20 +0100

    Constants & Functions
    Predefined constants and functions enum along with their nodes.

---
+++ src/main/kotlin/am/alite/ga/ast/Functions.kt
@@ -0,0 +1,5 @@
+package am.alite.ga.ast
+
+enum class Functions {
+    SQRT, CBRT, NROOT, LOG, LOGN, LN
+}
--- src/main/kotlin/am/alite/ga/ast/Nodes.kt
+++ src/main/kotlin/am/alite/ga/ast/Nodes.kt
@@ -7,7 +7,10 @@ sealed class node {
     fun div(other: node) = operatorNode('/', this, other)
 }
 
-class intNode(val value:Long):node
-class floatNode(val value:Double):node
-class operatorNode(val operator:Char, val left:node, val right:node)
+class intNode(val value: Long):node
+class floatNode(val value: Double):node
+class operatorNode(val operator: Char, val left: node, val right: node)
 class varNode(val id: String)
+class functionNode(val functionName: Functions, val arg: node)
+class biFunctionNode(val functionName: Functions, val arg1: node, val arg2: node)
+class constantNode(val id: Constants)
