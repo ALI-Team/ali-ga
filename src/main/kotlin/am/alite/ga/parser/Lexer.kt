7c7519aa5d82c5dc7ea0ea845a4d4eab3f17de15
AxelMatstoms <axel.matstoms@gmail.com>
2017-11-20 21:22:57 +0100

    Removed parser directory

--- src/main/kotlin/am/alite/ga/parser/Lexer.kt
+++
@@ -1,8 +0,0 @@
-package am.alite.ga.parser
-
-interface Lexer {
-    fun next(): Token
-    fun peek(): Token
-    fun eof(): Boolean
-    fun error(msg: String)
-}
--- src/main/kotlin/am/alite/ga/parser/Token.kt
+++
@@ -1,8 +0,0 @@
-package am.alite.ga.parser
-
-sealed class Token(val value: String)
-
-class NumberToken(value: String) : Token(value)
-class OperatorToken(value: String) : Token(value)
-class IdToken(value: String) : Token(value)
-class PuncToken(value: String) : Token(value)
